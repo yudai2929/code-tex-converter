9d67ee8d84fdc05c35b8a6ab1dd3a06898930995
yudai2929 <yudai201729@icloud.com>
2024-01-13 17:58:24 +0900

    add: release workflow

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+	"github.com/yudai2929/code-tex-converter/pkg/filewalker"
+	"github.com/yudai2929/code-tex-converter/pkg/latex"
+	"github.com/yudai2929/code-tex-converter/pkg/utils"
+)
+
+func main() {
+	cmd := &cli.Command{
+		Name:  "code-tex-converter ",
+		Usage: "convert code to latex listing format",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "target",
+				Usage:    "laTeX listing format target directory",
+				Aliases:  []string{"t"},
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:    "base",
+				Value:   "",
+				Usage:   "base path",
+				Aliases: []string{"b"},
+			},
+			&cli.BoolFlag{
+				Name:    "lstinput",
+				Value:   false,
+				Usage:   "use lstinputlisting",
+				Aliases: []string{"i"},
+			},
+		},
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			dir := cmd.String("target")
+			islLtInput := cmd.Bool("lstinput")
+			basePath := cmd.String("base")
+
+			resolvedBasePath := utils.TrimPath(basePath)
+
+			paths, err := filewalker.Walk(dir)
+			if err != nil {
+				return err
+			}
+
+			texListings, err := latex.Convert(paths, islLtInput, dir, resolvedBasePath)
+
+			if err != nil {
+				return err
+			}
+
+			displayTexListings(texListings)
+
+			return nil
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func displayTexListings(texListings []string) {
+	for _, texListing := range texListings {
+		fmt.Println(texListing)
+	}
+}
